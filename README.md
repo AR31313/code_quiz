c98d0f830a6ce5ec099f5d8de331623900617631
AR31313 <gurlgotgame@gmail.com>
2022-03-15 20:21:06 -0700

    Updated index

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# code_quiz
+# Web APIs: Code Quiz
+
+## Task
+ To build a timed coding quiz with multiple-choice questions. 
+ This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code. 
+  
+
+## The Application
+* Has a clean, polished, and responsive user interface.
+* A timer starts upon clicking the START button and user presented with a question.
+* Questions that follows up with the next question.
+* Incorrect answers will decrease the time left in the clock.
+* Game is over when all questions are answered or the timer reaches 0.
+* User then can save their initials and their score.
+
+* Application user experience is intuitive and easy to navigate.
+* Application user interface style is clean and polished.
+```
+---
+
+* Application GitHub URL: 
+https://github.com/AR31313/code_quiz
+
+* Live URL of the functional, deployed application: 
+https://ar31313.github.io/code_quiz/
+
+* Screenshot of the deployed application:
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+var myQuestions = [
+    {
+      question: "Which day is Earth Day?",
+      answers: {
+        a: 'April 22',
+        b: 'December 5',
+        c: '115th day of the Year',
+        d: 'January 1'
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "What country produces the most energy in the world?",
+      answers: {
+        a: 'China',
+        b: 'Iraq',
+        c: 'USA',
+        d: 'Geenland'
+      },
+      correctAnswer: 'a'
+    },
+    {
+        question: "Which substance makes up 78% of air?",
+        answers: {
+          a: 'Noble Gases',
+          b: 'Nitrogen',
+          c: 'Carbon Dioxide',
+          d: 'Oxygen'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Water is found in which of the following:",
+        answers: {
+          a: 'Plants',
+          b: 'Animals',
+          c: 'Fog',
+          d: 'All of the above'
+        },
+        correctAnswer: 'd'
+    }
+  ];
+  
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+
+  //create a function to generate a quiz.
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+  
+    function showQuestions(questions, quizContainer){
+      // local.Storage. GET OR SET?
+      var output = [];
+      var answers;
+  
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+        
+        // first reset the list of answers
+        answers = [];
+  
+        // for each available answer...
+        for(letter in questions[i].answers){
+  
+          // ...add an html radio button
+          answers.push(
+            '<label>'
+              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+              + letter + ': '
+              + questions[i].answers[letter]
+            + '</label>'
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          '<div class="question">' + questions[i].question + '</div>'
+          + '<div class="answers">' + answers.join('') + '</div>'
+        );
+      }
+  
+      // finally combine our output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+  
+    function showResults(questions, quizContainer, resultsContainer){
+      
+      // gather answer containers from our quiz
+      var answerContainers = quizContainer.querySelectorAll('.answers');
+      
+      // keep track of user's answers
+      var userAnswer = '';
+      var numCorrect = 0;
+      
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+  
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+        // if answer is correct
+        if(userAnswer===questions[i].correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+          
+          // color the answers green
+          answerContainers[i].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[i].style.color = 'red';
+        }
+      }
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+  
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
+  
+  }
