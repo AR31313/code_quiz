d1119e2f5052fc6866f27d4244fa2ea79be58764
AR31313 <gurlgotgame@gmail.com>
2022-03-16 17:50:43 -0700

    week 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,135 +1,91 @@
-
-var myQuestions = [
-    {
-      question: "Which day is Earth Day?",
-      answers: {
+//Questions for the quiz
+var myQuestion1 = {
+    question: "Which day is Earth Day?",
+    answer: {
         a: 'April 22',
         b: 'December 5',
         c: '115th day of the Year',
         d: 'January 1'
-      },
-      correctAnswer: 'a'
     },
-    {
-      question: "What country produces the most energy in the world?",
-      answers: {
+    correctAnswer: 'a'
+};
+var myQuestion2 = {
+    question: "What country produces the most energy in the world?",
+    answer: {
         a: 'China',
         b: 'Iraq',
         c: 'USA',
         d: 'Geenland'
-      },
-      correctAnswer: 'a'
     },
-    {
-        question: "Which substance makes up 78% of air?",
-        answers: {
-          a: 'Noble Gases',
-          b: 'Nitrogen',
-          c: 'Carbon Dioxide',
-          d: 'Oxygen'
-        },
-        correctAnswer: 'b'
+    correctAnswer: 'a'
+};
+var myQuestion3 = {
+    question: "Which substance makes up 78% of air?",
+    answer: {
+        a: 'Noble Gases',
+        b: 'Nitrogen',
+        c: 'Carbon Dioxide',
+        d: 'Oxygen'
     },
-    {
-        question: "Water is found in which of the following:",
-        answers: {
-          a: 'Plants',
-          b: 'Animals',
-          c: 'Fog',
-          d: 'All of the above'
-        },
-        correctAnswer: 'd'
-    }
-  ];
-  
-  var quizContainer = document.getElementById('quiz');
-  var resultsContainer = document.getElementById('results');
-  var submitButton = document.getElementById('submit');
-// on start, show the quiz
-startButton.onclick = function(){
-    myQuestions(questions, quizContainer);
-  }
-  //create a function to generate a quiz.
-  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-  
-  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-  
-    function showQuestions(questions, quizContainer){
-      // local.Storage. GET OR SET?
-      var output = [];
-      var answers;
-  
-      // for each question...
-      for(var i=0; i<questions.length; i++){
-        
-        // first reset the list of answers
-        answers = [];
-  
-        // for each available answer...
-        for(letter in questions[i].answers){
-  
-          // ...add an html radio button
-          answers.push(
-            '<label>'
-              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-              + letter + ': '
-              + questions[i].answers[letter]
-            + '</label>'
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          '<div class="question">' + questions[i].question + '</div>'
-          + '<div class="answers">' + answers.join('') + '</div>'
-        );
-      }
-  
-      // finally combine our output list into one string of html and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-  
-    function showResults(questions, quizContainer, resultsContainer){
-      
-      // gather answer containers from our quiz
-      var answerContainers = quizContainer.querySelectorAll('.answers');
-      
-      // keep track of user's answers
-      var userAnswer = '';
-      var numCorrect = 0;
-      
-      // for each question...
-      for(var i=0; i<questions.length; i++){
-  
-        // find selected answer
-        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-        
-        // if answer is correct
-        if(userAnswer===questions[i].correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-          
-          // color the answers green
-          answerContainers[i].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[i].style.color = 'red';
-        }
-      }
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-  
-    // show questions right away
-    showQuestions(questions, quizContainer);
+    correctAnswer: 'b'
+};
+var myQuestion4 = {
+    question: "Water is found in which of the following:",
+    answer: {
+        a: 'Plants',
+        b: 'Animals',
+        c: 'Fog',
+        d: 'All of the above'
+    },
+    correctAnswer: 'd'
+};
+
+var questions = [myQuestion1, myQuestion2, myQuestion3, myQuestion4]
+var answers = [myQuestion1, myQuestion2, myQuestion3, myQuestion4];
+var userAnswer = '';
+var numCorrect = 0;
+
+// for (let i = 0; i < questions.length; i++) {
+//     if(questions[i].correctAnswer == "b"){
+//         console.log("correct")
+//     }
     
-    // on submit, show results
-    submitButton.onclick = function(){
-      showResults(questions, quizContainer, resultsContainer);
-    }
-  
-  }
+// }
+
+var qi = 0 //questionIndex
+var ai = 0 //answerIndex
+var answerText =document.querySelector ("#answerText");
+var nextQuestion =document.querySelector ("#nextBtn");
+var startbutton = document.querySelector(".start-button");
+// var quizContainer = document.getElementById('quiz');
+var questionText = document.querySelector("#questionText");
+var resultsContainer = document.getElementById('results');
+// var submitButton = document.getElementById('submit');
+
+//display next question after clicking NEXT the button.
+nextQuestion.addEventListener("click", () => {
+    questionText.innerHTML=questions[qi].question;
+    qi++;
+});
+//display next answer after clicking NEXT the button.
+nextQuestion.addEventListener("click", () => {
+    answerText.innerHTML=questions[qi].answer;
+    qi++;
+});
+
+
+// // The startGame function is called when the start button is clicked
+// function startGame() {
+//     isWin = false;
+//     timerCount = 10;
+//     // Prevents start button from being clicked when round is in progress
+//     startbutton.disabled = true;
+//     // renderBlanks()
+//     questionText.innerHTML=questions[qi].question;
+//     qi++;
+//     // startTimer()
+// }
+// // on start, show the quiz
+// startbutton.addEventListener("click", startGame);
+// console.log("it works!");
+
