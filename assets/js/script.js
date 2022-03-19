c1ba5cfc96091dee0133fb048f5fd71e8f1e7e81
AR31313 <gurlgotgame@gmail.com>
2022-03-18 19:56:15 -0700

    start button'

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,10 +26,6 @@ var myQuestion4 =
 
 var questions = [myQuestion1, myQuestion2, myQuestion3, myQuestion4]
 // var answers = [myQuestion1, myQuestion2, myQuestion3, myQuestion4];
-var userAnswer = '';
-var numCorrect = 0;
-
-
 
 var qi = 0 //questionIndex
 var ai = 0 //answerIndex
@@ -38,30 +34,34 @@ var nextQuestion =document.querySelector ("#nextBtn");
 var startbutton = document.querySelector(".start-button");
 var questionText = document.querySelector("#questionText");
 var resultsContainer = document.getElementById('#results');
-var quizContainer = document.querySelector(".quizContainer");
-// var submitButton = document.getElementById('submit');
+// var quizContainer = document.querySelector(".quizContainer");
+// var submitButton = document.querySelector("#submitButton");
+// var resetButton = document.querySelector("#resetScore");
 
-//display the quiz upon hitting the start button
-startbutton.addEventListener("click", function (event) {
-    var element = event.target;
-    console.log("it works!1");
+// //display the quizContainer upon hitting the START button
+// startbutton.addEventListener("click", function (event) {
+//     var element = event.target;
+//     console.log("it works!1");
 
-// Use an if statement to conditionally display quiz
-    if (element.matches(".start-button")){
-        var state = element.getAttribute("data-view");
-        if (state==="hidden"){
-            //change the data-view attribute's value
-            element.setAttribute ("data-view","visible");
-            console.log("it works!2");
-    }   else {
-        // 'Hide' the number by setting .textContent to an empty string
-        element.textContent= "";
-        // Use .setAttribute() method
-        element.setAttribute("data-state", "hidden")
-        console.log("it works!3");
-        }
-    }
-});
+// // Use an if statement to conditionally display quiz
+//     if (element.matches(".start-button")){
+//         //set a variable named STATE that detects the state of an TARGET when EventListner hears "CLICK"
+//         var state =element.getAttribute("data-state");
+//         if (state==="hidden"){
+//             //change the data-number attribute's value
+//             element.textContent = element.dataset.number;
+//             // Change the attributes visible value (Named by ourselves) (all it does is toggles)
+//             element.setAttribute ("data-number","visible");
+//             console.log("it works!2");
+//     }   else {
+//         // 'Hide' the number by setting .textContent to an empty string
+//         element.textContent= "";
+//         // Use .setAttribute() method
+//         element.setAttribute("data-state", "hidden")
+//         console.log("it works!3");
+//         }
+//     }
+// });
 
 // //add eventlistner to the target container
 // quizContainer.addEventListener("click", function (event){
@@ -83,53 +83,67 @@ startbutton.addEventListener("click", function (event) {
 //     }
 // });
 
-function initTimer() {
-    timeLeft = 60;
-  }
+
 //display next question after clicking NEXT the button.
-nextQuestion.addEventListener("click", () => {
-    questionText.innerHTML=questions[qi].question;
-    qi++;
-    displayOptions();
-    console.log(qi);
+
+// startbutton.addEventListener("click", () => {
+//     questionText.innerHTML=questions[qi].question;
+//     qi++;
+//     displayOptions();
+//     console.log(qi);
+//     console.log(ai);
+// });
+
+
+//show & hide quiz
+var toggleBtn =document.querySelector('.start-button');
+var quizContainer = document.querySelector('.quizContainer');
+
+toggleBtn.addEventListener('click',() => {
+    if(quizContainer.style.display === "none") {
+    quizContainer.style.display = "block" ;
+    } else{
+        quizContainer.style.display === 'none';
+        console.log('it works!');
+    }
 });
 
-for (let i = 0; i < questions.length; i++) {
-    if(questions[0].correctAnswer == "0"){
-        console.log("correct")
-    }
-    
-}
-//function to display answer choices
-function displayOptions() {
-  //empty and create new button each time
-  answerText.innerHTML= "" 
-    var buttonA = document.createElement("button");
-    buttonA.textContent= questions[qi].answer[0];
-    // console.log(questions[qi].answer[ai]);
+// //function to display answer choices
 
-    var buttonB = document.createElement("button");
-    buttonB.textContent= questions[qi].answer[1];
-    // console.log(questions[qi].answer[0]);
+// function displayOptions() {
+//   //empty and create new button each time
+//   answerText.innerHTML= "" 
+//     var buttonA = document.createElement("button");
+//     buttonA.textContent= questions[qi].answer[0];
+//     console.log(questions[qi].answer[ai]);
 
-    var buttonC = document.createElement("button");
-    buttonC.textContent= questions[qi].answer[2];
-    // console.log(questions[qi].answer[1]);
+//     var buttonB = document.createElement("button");
+//     buttonB.textContent= questions[qi].answer[1];
+//     console.log(questions[qi].answer[ai]);
 
-    var buttonD = document.createElement("button");
-    buttonD.textContent= questions[qi].answer[3];
-    // console.log(questions[qi].answer[3]);
+//     var buttonC = document.createElement("button");
+//     buttonC.textContent= questions[qi].answer[2];
+//     console.log(questions[qi].answer[ai]);
 
-    answerText.appendChild(buttonA)
-    answerText.appendChild(buttonB)
-    answerText.appendChild(buttonC)
-    answerText.appendChild(buttonD)
-}
+//     var buttonD = document.createElement("button");
+//     buttonD.textContent= questions[qi].answer[3];
+//     console.log(questions[qi].answer[ai]);
+
+//     answerText.appendChild(buttonA)
+//     answerText.appendChild(buttonB)
+//     answerText.appendChild(buttonC)
+//     answerText.appendChild(buttonD)
+
+    // for (let i = 0; i < questions.length; i++) {
+    //     if(question.answer === correctAnswer){
+    //         console.log("correct")
+    //     }  
+    // }
+// }
+// var userAnswer = '';
+// var numCorrect = 0;
 
 
-// var submitBtn = document.querySelector("#submitBtn");
-// var goBackBtn = document.querySelector("#goBackBtn");
-// var clearScoresBtn = document.querySelector("#clearScoresBtn");
 
 // // The startGame function is called when the start button is clicked
 // function startGame() {
