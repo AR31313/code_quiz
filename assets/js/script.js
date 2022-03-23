66044c155f548b97069ed5b7bc090440651b7d08
AR31313 <gurlgotgame@gmail.com>
2022-03-23 11:32:44 -0700

    score keeper

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,46 +3,45 @@
 var qi = 0 //questionIndex
 var ai = 0 //answerIndex
 var userInitial= [];
-const startingMinutes = 1;
-let time = startingMinutes *60;
+let time = 60;
+var numCorrect = 0;
 
 const countdownEL =document.getElementById('countdown');
-setInterval (updatedCountdown,1000);
+let interval = setInterval (updatedCountdown,1000);
 //function called updatedCountdown that takes seconds as an argument:
 function updatedCountdown (){
-    let seconds = time % 60;
-    countdownEL.innerHTML =`${seconds}`;
-    time--;
     //if statement that executes when counter is less than 0:
-         if (time < 0 ) {
-             clearInterval(startingMinutes);
+         if (time <= 0 ) {
+             clearInterval(interval);
             console.log('Ding! Time is UP');
           }
+    countdownEL.innerHTML =`${time}`;
+    time--;
 }
 
 var myQuestion1 = 
     {
     question: "Which day is Earth Day?",
     answer: ["April 22","December 5","115th day of the Year","January 1"],
-    correctAnswer: "0"
+    correctAnswer: "April 22"
 };
 var myQuestion2 = 
     {
     question: "What country produces the most energy in the world?",
     answer: ['China','Iraq','USA','Geenland'],
-    correctAnswer: '0'
+    correctAnswer: 'China'
 };
 var myQuestion3 = 
     {
     question: "Which substance makes up 78% of air?",
     answer: ['Noble Gases','Nitrogen','Carbon Dioxide','Oxygen'],
-    correctAnswer: '1'
+    correctAnswer: 'Nitrogen'
 };
 var myQuestion4 =
     {
     question: "Water is found in which of the following:",
     answer: ['Plants','Animals','Fog','All of the above'],
-    correctAnswer: '3'
+    correctAnswer: 'All of the above'
 };
 
 var questions = [myQuestion1, myQuestion2, myQuestion3, myQuestion4]
@@ -52,6 +51,7 @@ var quizContainer = document.querySelector(".quizContainer");
 var answerText =document.querySelector ("#answerText");
 var questionText = document.querySelector("#questionText");
 var resultsContainer = document.getElementById('#results');
+var score = document.querySelector('.score'); //a variable to store users Answer Choice
 
 // var submitButton = document.querySelector("#submitButton");
 // var resetButton = document.querySelector("#resetScore");
@@ -59,6 +59,7 @@ var resultsContainer = document.getElementById('#results');
 // //display the quizContainer upon hitting the START button
 var toggleBtn =document.querySelector('.start-button'); 
 quizContainer.style.display = "none"
+
 
 
 //show the quiz after hitting 'Start' Button
@@ -70,9 +71,15 @@ toggleBtn.addEventListener('click',() => {
         toggleBtn.innerHTML="RESTART";
         console.log('it works!');
     }
+
+    questionText.innerHTML=questions[qi].question;
+    displayOptions();
+    console.log(qi);
+    console.log(ai);
+
 });
 
-//display next questions after clicking SHOWQUESTION the button.
+//display next questions after clicking SHOWQUESTION button.
 nextQuestion.addEventListener("click", () => {
     questionText.innerHTML=questions[qi].question;
     displayOptions();
@@ -82,6 +89,11 @@ nextQuestion.addEventListener("click", () => {
 
 //function to display answer choices
 function displayOptions() {
+
+    if (time <= 0 ) {
+        qi = 100
+        }
+
   //empty and create new button each time
   answerText.innerHTML= "" 
     var buttonA = document.createElement("button");
@@ -107,53 +119,72 @@ function displayOptions() {
 
 // when the user clicks on button,then check if answer is correct
     buttonA.addEventListener('click', function (event) {
-        var userAnswer = event.target;
-        if(userAnswer === questions[qi].correctAnswer){
-            console.log("event")
-        }  else (console.log("Correct!"))
-        return;
+        var userAnswer = event.target.innerHTML;
+        console.log( questions[qi - 1].correctAnswer)
+        if(userAnswer === questions[qi -1].correctAnswer){
+            numCorrect += 10;
+            score.innerHTML = numCorrect
+            console.log("Correct +10pts")
+        }  else { //if the answer is incorrect, then ALERT the user. 
+            console.log("incorrect!")
+            time -= 10; //then take away 10 seconds from the timer.
+            numCorrect -= 10; //then take away 10 points from the Score.
+            score.innerHTML = numCorrect
+        }
     });
     buttonB.addEventListener('click', function (event) {
         var userAnswer = event.target;
-        if(userAnswer === questions[qi].correctAnswer){
-            console.log("correct3")
-        }  else (console.log("Correct!"))
-        return;
+        console.log(event.target.innerHTML)
+        if(userAnswer === questions[qi- 1].correctAnswer){
+            numCorrect += 10;
+            score.innerHTML = numCorrect
+            console.log("Correct +10pts")
+
+        }  else {
+            console.log("incorrect!")
+            time -= 10;
+            numCorrect -= 10;
+            score.innerHTML = numCorrect
+        }
     });
     buttonC.addEventListener('click', function (event) {
         var userAnswer = event.target;
-        if(userAnswer === questions[qi].correctAnswer){
-            console.log("correct4")
-        }  else (console.log("Correct!"))
+        if(userAnswer === questions[qi- 1].correctAnswer){
+            numCorrect += 10;
+            score.innerHTML = numCorrect
+            console.log("Correct +10pts")
+        }  else {
+            console.log("incorrect!")
+            time -= 10;
+            numCorrect -= 10;
+            score.innerHTML = numCorrect
+        }
         return;
     });
     buttonD.addEventListener('click', function (event) {
         var userAnswer = event.target;
-        if(userAnswer === questions[qi].correctAnswer){
-            console.log("correct")
-        }  else (console.log("Correct!"))
+        if(userAnswer === questions[qi- 1].correctAnswer){
+            console.log("Correct +10pts")
+        }  else {
+            console.log("incorrect!")
+            time -= 10;
+            numCorrect -= 10;
+            score.innerHTML = numCorrect
+        }
         return;
     });
 }
 
-//if the answer is incorrect, then ALERT the user, then take away 10 seconds from the timer.
-
-//if the answer is correct, then load next q's & a's.
-//a variable to store users Answer Choice
-var userAnswer = '';
-
-//repeat last process until all questions are asked.
-
-// when user answers last question, then hide quizContainer, then stop coutdown timer.
-
-//a variable to store the answers that are correct
-var numCorrect = 0;
+// when user answers last question, then hide quizContainer & stop coutdown timer.
 
 //score is submitted via SUBMIT SCORE btn or score is reset to 0-0 via RESET SCORE btn.
+
 //if user clicks SUBMIT SCORE btn then display a form input to collect user initials.
+
 //if user clicks RESET SCORE btn then hide the quizConatiner again(& START the Game again)
 
 // when the user clicks submit button, then open FORM to save initials+ score & STORE to localStorage.
+
 // if user submits their Initials then hide the quizcontainer again after they click OK TO submit & START the Game again
 
 // //Initializing of the Game
